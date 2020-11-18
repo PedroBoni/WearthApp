765130c87dbf68820a4126fc0232badb58f5ff2d
PedroBoni <pedro.henrique1210@outlook.com>
2020-11-19 08:36:13 +0900

    Project's Initial Commit

---
+++ www/components/index.js
@@ -0,0 +1,142 @@
+const conditionData = [{ "condition": "Tempestade", "day": "https://assets5.lottiefiles.com/private_files/lf30_kj3arjju.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_22gtsfnq.json" }, { "condition": "Tempestade", "day": "https://assets5.lottiefiles.com/private_files/lf30_kj3arjju.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_22gtsfnq.json" }, { "condition": "Furacão", "day": "https://assets5.lottiefiles.com/private_files/lf30_kj3arjju.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_22gtsfnq.json" }, { "condition": "Tempestade", "day": "https://assets5.lottiefiles.com/private_files/lf30_kj3arjju.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_22gtsfnq.json" }, { "condition": "Tempestade", "day": "https://assets5.lottiefiles.com/private_files/lf30_kj3arjju.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_22gtsfnq.json" }, { "condition": "Neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Chuva", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Geada fina", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Chuva", "day": "https://assets3.lottiefiles.com/private_files/lf30_rb778uhf.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_jr9yjlcf.json" }, { "condition": "Chuva", "day": "https://assets3.lottiefiles.com/private_files/lf30_rb778uhf.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_jr9yjlcf.json" }, { "condition": "Chuva", "day": "https://assets3.lottiefiles.com/private_files/lf30_rb778uhf.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_jr9yjlcf.json" }, { "condition": "Chuva", "day": "https://assets3.lottiefiles.com/private_files/lf30_rb778uhf.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_jr9yjlcf.json" }, { "condition": "Neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Tempestade com neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Granizo", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Gelo", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Poeira", "day": "https://assets6.lottiefiles.com/private_files/lf30_moaf5wp5.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_iugenddu.json" }, { "condition": "Neblina", "day": "https://assets2.lottiefiles.com/private_files/lf30_jvkyx6tg.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_qqhrsksk.json" }, { "condition": "Tempestade de areia", "day": "https://assets2.lottiefiles.com/private_files/lf30_jvkyx6tg.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_qqhrsksk.json" }, { "condition": "Fumacento", "day": "https://assets2.lottiefiles.com/private_files/lf30_jvkyx6tg.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_qqhrsksk.json" }, { "condition": "Vento acentuado", "day": "https://assets2.lottiefiles.com/private_files/lf30_jvkyx6tg.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_qqhrsksk.json" }, { "condition": "Ventania", "day": "https://assets2.lottiefiles.com/private_files/lf30_jvkyx6tg.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_qqhrsksk.json" }, { "condition": "Tempo frio", "day": "https://assets7.lottiefiles.com/private_files/lf30_ykkzuozu.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_5tzqguri.json" }, { "condition": "Nublado", "day": "https://assets7.lottiefiles.com/private_files/lf30_ykkzuozu.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_5tzqguri.json" }, { "condition": "Tempo limpo", "day": "https://assets4.lottiefiles.com/private_files/lf30_moaf5wp5.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_iugenddu.json" }, { "condition": "Nublado", "day": "https://assets7.lottiefiles.com/private_files/lf30_ykkzuozu.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_5tzqguri.json" }, { "condition": "Parcialmente nublado", "day": "https://assets2.lottiefiles.com/private_files/lf30_j1g2rpsv.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_sgiztka9.json" }, { "condition": "Parcialmente nublado", "day": "https://assets2.lottiefiles.com/private_files/lf30_j1g2rpsv.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_sgiztka9.json" }, { "condition": "Tempo limpo", "day": "https://assets4.lottiefiles.com/private_files/lf30_moaf5wp5.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_iugenddu.json" }, { "condition": "Ensolarado", "day": "https://assets4.lottiefiles.com/private_files/lf30_moaf5wp5.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_iugenddu.json" }, { "condition": "Estrelado", "day": "https://assets2.lottiefiles.com/private_files/lf30_iugenddu.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_iugenddu.json" }, { "condition": "Ensolarado", "day": "https://assets7.lottiefiles.com/private_files/lf30_ykkzuozu.json", "night": "https://assets7.lottiefiles.com/private_files/lf30_ykkzuozu.json" }, { "condition": "Chuva e granizo", "day": "https://assets5.lottiefiles.com/private_files/lf30_kj3arjju.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_22gtsfnq.json" }, { "condition": "Ar quente", "day": "https://assets4.lottiefiles.com/private_files/lf30_moaf5wp5.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_iugenddu.json" }, { "condition": "Tempestades isoladas", "day": "https://assets2.lottiefiles.com/private_files/lf30_oj6pxozf.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_3udf8lcw.json" }, { "condition": "Trovoadas dispersas", "day": "https://assets2.lottiefiles.com/private_files/lf30_oj6pxozf.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_3udf8lcw.json" }, { "condition": "Trovoadas dispersas", "day": "https://assets2.lottiefiles.com/private_files/lf30_oj6pxozf.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_3udf8lcw.json" }, { "condition": "Chuvas esparsas", "day": "https://assets2.lottiefiles.com/private_files/lf30_oj6pxozf.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_3udf8lcw.json" }, { "condition": "Neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Chuviscos com neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Tempo limpo", "day": "https://assets2.lottiefiles.com/private_files/lf30_j1g2rpsv.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_j1g2rpsv.json" }, { "condition": "Chuva", "day": "https://assets2.lottiefiles.com/private_files/lf30_oj6pxozf.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_3udf8lcw.json" }, { "condition": "Neve", "day": "https://assets3.lottiefiles.com/private_files/lf30_w5u9xr3a.json", "night": "https://assets3.lottiefiles.com/private_files/lf30_9bptg8sh.json" }, { "condition": "Tempestades isoladas", "day": "https://assets2.lottiefiles.com/private_files/lf30_oj6pxozf.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_3udf8lcw.jso" }, { "condition": "Indisponivel", "day": "https://assets4.lottiefiles.com/private_files/lf30_moaf5wp5.json", "night": "https://assets2.lottiefiles.com/private_files/lf30_iugenddu.json" }];
+
+const conditionDataSample = [
+  {type: "storm", ptBR: "Tempestade", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/stormy-weather/stormy-weather.json"},
+  {type: "snow", ptBR: "Neve", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/snow-storm-weather/snow-storm-weather.json"},
+  {type: "hail", ptBR: "Granizo", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/torrential-rain-weather/torrential-rain-weather.json"},
+  {type: "rain", ptBR: "Chuva", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/rainy-weather/rainy-weather.json"},
+  {type: "fog", ptBR: "Neblina", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/haze-weather/haze-weather.json"},
+  {type: "clear_day", ptBR: "Limpo", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/day-night-weather/day-night-weather.json"},
+  {type: "clear_night", ptBR: "Limpo", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/day-night-weather/day-night-weather.json"},
+  {type: "cloud", ptBR: "Nublado", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/cloudy-weather/cloudy-weather.json"},
+  {type: "cloudly_day", ptBR: "Nublado", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/cloudy-weather/cloudy-weather.json"},
+  {type: "cloudly_night ", ptBR: "Nublado", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/cloudy-weather/cloudy-weather.json"},
+  {type: "none_day", ptBR: "Limpo", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/day-night-weather/day-night-weather.json"},
+  {type: "none_night", ptBR: "Limpo", animation: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/day-night-weather/day-night-weather.json"},
+]
+
+
+const weatherTodayH1 = document.querySelector("#weatherToday #value h1");
+const city = document.querySelector("#city");
+const date = document.querySelector("#date");
+const desc = document.querySelector("#desc");
+const weatherToday = document.querySelector("#weatherToday");
+const windValue = document.querySelector("#wind #value");
+const humidityValue = document.querySelector("#humidity #value");
+const weatherDays = document.querySelector("#weatherDays");
+const load = document.querySelector("#load");
+document.addEventListener("deviceready", () => {
+  let reload = -1;
+  
+  function loadDate() {
+    reload++;
+
+    const weatherTodayAnimation = document.querySelector("#weatherToday #animation");
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+    function onSuccess(position) {
+      response({ latitude: position.coords.latitude,
+                    longitude: position.coords.longitude });
+    };
+    function onError(error) {
+      response({ latitude: -23.540993, 
+                  longitude: -46.631296 })
+    }   
+    function response(userLocation){
+      fetch(
+      ` https://api.hgbrasil.com/weather?key=c78294d9&lat=${userLocation.latitude}&lon=${userLocation.longitude}`
+      ).then((response) => {
+        return response.json().then((json) => {
+          weatherTodayH1.innerText = json.results.temp;
+          city.innerText = json.results.city;
+          date.innerText = formatDate(json.results.date, 1);
+          desc.innerText = conditionData[json.results.condition_code].condition;
+          if (!weatherTodayAnimation)
+            weatherToday.insertAdjacentHTML('afterbegin', insertAnimation(conditionData[json.results.condition_code].day))
+          else
+            weatherTodayAnimation.src = conditionData[json.results.condition_code].day;
+
+
+          windValue.innerText = json.results.wind_speedy;
+          humidityValue.innerText = json.results.humidity + "%";
+
+          if(reload == 0){
+            for(let i = 0; i < (json.results.forecast).length; i++){
+              weatherDays.insertAdjacentHTML('beforeend', insertDay(json.results.forecast[i], formatDate(json.results.date, 3)))
+            }
+            setTimeout(() => load.remove(), 1500);              
+          }                   
+        });
+      });
+    }  
+  }
+ 
+  setInterval(loadDate, 10000);
+
+}, false);
+// utils
+function formatDate(date, type) {
+  let msec = Date.parse(date);
+  let longDate = new Date(msec);
+  let month = longDate.toLocaleDateString('pt-br', { month: 'long' });
+  let day = longDate.toLocaleDateString('pt-br', { day: 'numeric' });
+  let weekday = longDate.toLocaleDateString('pt-br', { weekday: 'long' });
+  let year = longDate.toLocaleDateString('pt-br', { year: 'numeric' });
+
+  if(type == 1)
+    return capitalizeFirstLetter(weekday.substring(0, 3)) + ", " + day + " " + capitalizeFirstLetter(month.substring(0, 3));
+  else if(type == 2)
+    return capitalizeFirstLetter(weekday.substring(0, 3));
+  else if(type == 3)
+    return year;
+  else if(type == 4)
+    return day;
+}
+
+function capitalizeFirstLetter(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function insertAnimation(jsonAnimation) {
+  return `<lottie-player src="${jsonAnimation}" background="transparent" speed="1" style="width: 9rem" loop autoplay id="animation"></lottie-player>`
+}
+function insertDay(data, year){
+  // let year = formatDate(data.date)
+  let condition = getCodition(data.condition);
+  let averageTemperature = (data.max + data.min) / 2;
+  let day = (data.date).substring(0, 2);
+  let month = (data.date).substring(3, 5);
+
+  let date = (data.date).indexOf("/") == -1 ? (year + "-" + data.date) : (year + "-" + month + "-" + day);
+
+  let html = `<div class="day">
+        <p id="weekday">${formatDate(date, 2)}</p>
+        <p id="dayNum">${formatDate(date, 4)}</p>
+        <lottie-player
+          src="${condition.animation}"
+          background="transparent"
+          speed="1"
+          style="width: 3rem"
+          loop
+          autoplay
+          id="animation"
+        ></lottie-player>
+        <p id="value">${Math.round(averageTemperature)}°</p>
+      </div>`
+    
+    return html;
+}
+
+function getCodition(condition){
+  let res;
+  for(let i = 0; i < conditionDataSample.length; i++){
+    if(condition == conditionDataSample[i].type){
+      res = {
+        ptBR: conditionDataSample[i].ptBR,
+        animation: conditionDataSample[i].animation
+      }
+      return res;
+    }
+  }
+}
